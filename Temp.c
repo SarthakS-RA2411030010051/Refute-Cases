b56c89e9de8234727276a6a488bbdf5b485e4356
Sarthak Srivastava <ss6883@srmist.edu.in>
2024-12-09 01:49:21 +0530

    Update Temp.c

--- Temp.c
+++ Temp.c
@@ -11,27 +11,31 @@ int main()
     printf("Enter a unit of temperature (C, F, K): ");
     scanf(" %c", &unit);
     //Convert the temperature to the other two units of temperature
-    if (unit == 'C')
-    {
-        tempC = temp;
-        tempF = temp * 9 / 5 + 32;
-        tempK = temp + 273.15;
+    if (unit == 'C'||unit == 'F'||unit == 'K'){
+        if (unit == 'C')
+        {
+            tempC = temp;
+            tempF = temp * 9 / 5 + 32;
+            tempK = temp + 273.15;
+        }
+        else if (unit == 'F')
+        {
+            tempC = (temp - 32) * 5 / 9;
+            tempF = temp;
+            tempK = temp + 273.15;
+        }
+        else if (unit == 'K')
+        {
+            tempC = temp - 273.15;
+            tempF = (tempC * 9 / 5) + 32;
+            tempK = temp;
+        }
+        //Display the converted temperatures
+        printf("Temperature in Celsius: %.2f\n", tempC);
+        printf("Temperature in Fahrenheit: %.2f\n", tempF);
+        printf("Temperature in Kelvin: %.2f\n", tempK);
+    } else {
+        printf("Invalid Unit.")
     }
-    else if (unit == 'F')
-    {
-        tempC = (temp - 32) * 5 / 9;
-        tempF = temp;
-        tempK = temp + 273.15;
-    }
-    else if (unit == 'K')
-    {
-        tempC = temp - 273.15;
-        tempF = (tempC * 9 / 5) + 32;
-        tempK = temp;
-    }
-    //Display the converted temperatures
-    printf("Temperature in Celsius: %.2f\n", tempC);
-    printf("Temperature in Fahrenheit: %.2f\n", tempF);
-    printf("Temperature in Kelvin: %.2f\n", tempK);
     return 0;
 }
